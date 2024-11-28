25073ad7a44c6fc7f0fe040419ba9e97db447dbf
Mahi <mforjava@gmail.com>
2024-11-28 07:43:59 +0000

    sample

--- SpringBootDockerDemo/src/main/java/com/mahi/practise/SpringBootDockerDemoApplication.java
+++ SpringBootDockerDemo/src/main/java/com/mahi/practise/SpringBootDockerDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDockerDemoApplication {
 
 	public static void main(String[] args) {
+//
 		SpringApplication.run(SpringBootDockerDemoApplication.class, args);
 	}
 
